6937e13ae1f8bdb2ef98aeb5bf8f80f4ef20691c
halink0803 <halink0803@gmail.com>
2024-11-19 16:59:21 +0700

    update event manager and memship manager

--- src/core/EventManager.sol
+++ src/core/EventManager.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { MembershipManager } from "./MembershipManager.sol";
 
 contract EventManager is Ownable {
-    uint256 private eventId;
+    uint256 public eventIds;
     MembershipManager public memberShipManager;
     using SafeERC20 for ERC20;
 
@@ -18,12 +18,6 @@ contract EventManager is Ownable {
     mapping(uint256=>address[]) public registeredUsers;
 
     mapping(uint256=>Event) public events;
-
-    enum Tier {
-        Bronze,
-        Silver,
-        Gold
-    }
 
     /**
      * struct to define an event
@@ -38,7 +32,7 @@ contract EventManager is Ownable {
         string name;
         uint256 ticketPrice;
         address currency;
-        tier minimumTierRequired;
+        MembershipManager.Tier minimumTierRequired;
     }
 
     event EventCreated(uint256 eventId, string name);
@@ -46,50 +40,50 @@ contract EventManager is Ownable {
     event RegisterSuccess(uint256 eventId, address user);
 
     constructor(address owner, address _memberShipManager) Ownable(owner) {
-        memberShipManager = _memberShipManager;
+        memberShipManager = MembershipManager(_memberShipManager);
     }
 
     /**
      * @notice create new event, only the owner of EventManager contract can create new event 
      * @param name event name
      */
-    function createEvent(string calldata name, uint256 ticketPrice, adderss currency, Tier minimumTierRequired) public onlyOwner{
+    function createEvent(string calldata name, uint256 ticketPrice, address currency, MembershipManager.Tier minimumTierRequired) public onlyOwner{
         // TODO: check currency is a ERC20 token
-        eventId++;
-        ev = Event(eventId, name, ticketPrice, currency, minimumTierRequired);
-        emit EventCreated(eventId, name);
+        eventIds++;
+        events[eventIds] = Event(eventIds, name, ticketPrice, currency, minimumTierRequired);
+
+        emit EventCreated(eventIds, name);
     }
 
     /**
      * register to join an event 
      * if user have a membershipNFT, they can register
      * otherwise they have to buy a single ticket
-     * @param eventId event id to register
+     * @param _eventId event id to register
      */
     function register(uint256 _eventId) public {
-        require(_eventId < eventId, "The event does not exists");
-        ev = events[eventId];
+        require(_eventId < eventIds, "The event does not exists");
+        Event memory ev = events[_eventId];
 
         require(memberShipManager.checkMembership(msg.sender, ev.minimumTierRequired), "user without membership cannot register");
 
-        users = registeredUsers[eventId];
-        users.push(msg.sender);
+        registeredUsers[_eventId].push(msg.sender);
 
-        emit RegisterSuccess(eventId, msg.sender);
+        emit RegisterSuccess(_eventId, msg.sender);
     }
 
     /**
      * buy a ticket if you don't have a membership 
-     * @param eventId event id to buy ticket
+     * @param _eventId event id to buy ticket
      */
-    function buyTicket(uint256 eventId) public {
-        ev = events[eventId];
+    function buyTicket(uint256 _eventId) public {
+        Event memory ev = events[_eventId];
         // if event is free to join, just add them to the list
         if (ev.ticketPrice > 0) {
             ERC20(ev.currency).safeTransferFrom(msg.sender, address(this), ev.ticketPrice);
         }
-        users = registeredUsers[eventId];
-        users.push(msg.sender);
-        emit RegisterSuccess(eventId, msg.sender);
+        registeredUsers[_eventId].push(msg.sender);
+
+        emit RegisterSuccess(_eventId, msg.sender);
     }
 }
